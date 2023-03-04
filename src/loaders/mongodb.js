759ac44f31a21ff1b7206819e2d8ba403f6a8b96
FranciscoManuelLopezCabrera <franciscomanuel.lopez@adaits.es>
2023-03-04 19:28:09 +0100

    Update mongodb.js

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -5,5 +5,4 @@ module.exports = async config => {
     const{ user, pass, host, port, dbName} = config;
     await mongoose.connect(`mongodb+srv://${user}:${pass}@${host}${port ? `:${port}`:''}/${dbName}?retryWrites=true&w=majority`);
     console.log('🐸 CONECTADO 🐸');
-
 }
